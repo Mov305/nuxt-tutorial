2aed1ca0ac2019c7fbc7953ddbee420f488e6184
Mov305 <abd3052001@gmail.com>
2023-11-30 19:39:21 +0200

    tailwindcss Configered

--- app.vue
+++ app.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <h1>My App</h1>
-    <router-view>
-      <!-- component matched by the route will render here -->
-      h
-    </router-view>
+    <h1 class=" text-red-800">My App</h1>
+    <!-- component matched by the route will render here -->
   </div>
 </template>
